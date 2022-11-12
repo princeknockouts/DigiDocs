52117772ea3707e2252f2b16e837fd0f5d819f63
Prince Karania <karania.prince@gmail.com>
2022-11-12 21:06:37 +0530

    changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
-import { ScrollView, Text, StyleSheet } from "react-native";
+import { ScrollView, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 class Dashboard extends Component {
 	render() {
+		const { rootViewStyle } = styles;
 		return (
-			<ScrollView>
+			<ScrollView style={rootViewStyle}>
 				<Text>Dashboard</Text>
+				<TouchableOpacity
+					onPress={() => {
+						console.log("test");
+					}}
+				>
+					<Text>Test</Text>
+				</TouchableOpacity>
 			</ScrollView>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	rootViewStyle: {
+		flex: 1,
+		backgroundColor: "white",
+	},
+});
 
 export default Dashboard;
