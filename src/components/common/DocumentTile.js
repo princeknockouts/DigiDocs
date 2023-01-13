831341bebaeea2a40f9371dc7502c053e1d1f206
Prince Karania <karania.prince@gmail.com>
2023-01-13 19:56:31 +0530

    changes in component

--- src/components/common/DocumentTile.js
+++ src/components/common/DocumentTile.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import {
+	View,
+	TouchableOpacity,
+	Text,
+	StyleSheet,
+	ImageBackground,
+	Image,
+} from "react-native";
 
 class DocumentTile extends Component {
 	render() {
-		const { mainViewStyle } = styles;
+		const { mainViewStyle, imageStyle, innerViewStyle } = styles;
+		const { onPress, documentName, documentIssuer, dateOfIssue } =
+			this.props;
 		return (
-			<View>
-				<Text>Document Name</Text>
-				<Text>Org Name</Text>
-				<Text>DOI</Text>
-			</View>
+			<TouchableOpacity onPress={onPress}>
+				<ImageBackground
+					source={require("../../assets/icons/file.png")}
+					style={imageStyle}
+					tintColor="gray"
+				>
+					<View style={innerViewStyle}>
+						<Text style={{ width: 100, flexWrap: "wrap" }}>
+							{documentName}
+						</Text>
+
+						<Text>{documentIssuer}</Text>
+						<Image
+							source={require("../../assets/icons/image-test-icon.jpeg")}
+							style={{
+								height: 100,
+								width: 80,
+								alignSelf: "center",
+								marginBottom: 5,
+								marginTop: 5,
+							}}
+						/>
+						<Text>Date of Issue : {dateOfIssue}</Text>
+					</View>
+				</ImageBackground>
+			</TouchableOpacity>
 		);
 	}
 }
@@ -17,6 +47,16 @@ class DocumentTile extends Component {
 const styles = StyleSheet.create({
 	mainViewStyle: {
 		//
+	},
+	imageStyle: {
+		height: 200,
+		width: 160,
+		// backgroundColor: "blue",
+		tintColor: "lightgray",
+		marginTop: 20,
+	},
+	innerViewStyle: {
+		padding: 12,
 	},
 });
 
